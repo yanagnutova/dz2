dea49cfc038267a4215dbede67c7973d604eb554
yanagnutova <yanagnutova@icloud.com>
2023-01-22 20:35:43 +0400

    добавление первой задачи

---
+++ task21/Program.cs
@@ -0,0 +1,26 @@
+﻿// на входе трехзначное число, на выходе - вторая цифра
+
+int InputInt (string message)
+{
+    Console.Write (message + ">");
+    string? inputValue = Console.ReadLine ();
+    int result = Convert.ToInt32 (inputValue);
+    return result;
+}
+
+bool ValidateN3 (int Numb)
+{
+    if (Numb > 99 && Numb < 1000)
+    {
+        return true;
+    }
+    Console.WriteLine ("Число не трехзначное");
+    return false;
+}
+
+int Numb = InputInt ("Введите трехзначное число");
+if (ValidateN3 (Numb))
+{
+    int result = Numb / 10 % 10;
+    Console.WriteLine ($"Вторая цифра {Numb} = {result}");
+}
