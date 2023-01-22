ca618d2b61127d3973402f89ba8a7c53a791cd07
yanagnutova <yanagnutova@icloud.com>
2023-01-22 20:39:26 +0400

    Добавление второй задачи

---
+++ task22/Program.cs
@@ -0,0 +1,31 @@
+﻿// Вывод третьей цифры заданного числа, если такая существует
+
+
+int InputInt (string message)
+{
+    Console.Write (message + ">");
+    string? inputValue = Console.ReadLine ();
+    int result = Convert.ToInt32 (inputValue);
+    return result;
+}
+
+bool ValidateN3 (int Numb)
+{
+    if (Numb > 99)
+    {
+        return true;
+    }
+    Console.WriteLine ("Нет 3 цифры в числе");
+    return false;
+}
+
+int Numb = InputInt("Введите число > 99 ");
+if (ValidateN3(Numb))
+{
+    while (Numb > 999)
+    {
+        Numb = Numb / 10;
+    }
+    int result = Numb % 10;
+    System.Console.WriteLine($"Третья цифра числа {Numb} = {result}");
+}
